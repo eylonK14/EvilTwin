59a27c511684b0755057002bca1b6704acb18166
eylonK14 <eylony1403@gmail.com>
2025-05-12 12:05:47 -0400

    created initail AP/STA discovery method

--- evil_twin.py
+++ evil_twin.py
@@ -16,10 +16,11 @@ def get_channel(pkt):
     return None
 
 def callback(pkt):
+    print(pkt.show())
     if pkt.haslayer(Dot11):
         # Identify Access Points from Beacon/ProbeResp frames
-        if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
-            bssid = pkt[Dot11].addr3
+        if pkt.haslayer(Dot11Beacon):
+            bssid = pkt[Dot11].addr2
             ssid = pkt[Dot11Elt].info.decode(errors='ignore')
             channel = get_channel(pkt)
             if bssid and bssid not in aps:
@@ -44,55 +45,3 @@ def callback(pkt):
                 pass
 
 sniff(iface=interface, prn=callback, store=0)
-
-
-
-# def get_power_constraint(beacon):
-#     power_constraint_elem = beacon.getlayer(Dot11Elt, ID=32)
-#     if power_constraint_elem:
-#         power_constraint = ord(power_constraint_elem.info)
-#         return power_constraint
-#     return None
-
-# def parse_beacon_frames(packets):
-#     beacon_frames = packets.filter(lambda p: p.haslayer(Dot11Beacon))
-#     channel_info = {}
-#     for beacon in beacon_frames:
-#         ssid = beacon.info.decode('utf-8', 'ignore')
-#         bssid = beacon.addr3
-#         channel = ord(beacon[Dot11Elt:3].info)
-#         power_constraint = get_power_constraint(beacon)
-#         if channel not in channel_info:
-#             channel_info[channel] = []
-#         channel_info[channel].append((ssid, bssid, power_constraint))
-#     return channel_info
-
-# def parse_management_frames(packets):
-#     management_frames = packets.filter(lambda p: p.haslayer(Dot11) and p.type == 0)
-#     frame_info = []
-#     for frame in management_frames:
-#         if frame.subtype != 8:
-#             continue
-
-#         source_mac = frame.addr2
-#         destination_mac = frame.addr1
-#         frame_info.append((frame_type, source_mac, destination_mac))
-#     return frame_info
-
-# def get_frame_type_explanation(frame_type):
-#     frame_types = {
-#         0: "Association Request",
-#         1: "Association Response",
-#         2: "Reassociation Request",
-#         3: "Reassociation Response",
-#         4: "Probe Request",
-#         5: "Probe Response",
-#         8: "Beacon",
-#         9: "ATIM",
-#         10: "Disassociation",
-#         11: "Authentication",
-#         12: "Deauthentication",
-#         13: "Action"
-#     }
-#     return frame_types.get(frame_type, "Unknown")
-
